ed070eb0ec17a92f814812198e9a2cb4a0eab336
Chris <zucehao@163.com>
2024-05-24 17:28:21 +0800

    the last change

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, re_path
 urlpatterns = [
     path("", views.post_list, name="post_list"),
     re_path(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    re_path(r'^post/new/$', views.post_new, name='post_new'),
 ]
